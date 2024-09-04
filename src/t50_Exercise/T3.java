e14e2deae7ccc9680a485fb920e403e038949d17
MoonJanggyu <bricuerp@gmail.com>
2024-09-04 12:40:31 +0900

    20240904_1240 update 3

--- src/t50_Exercise/T3.java
+++ src/t50_Exercise/T3.java
@@ -2,8 +2,11 @@ package t50_Exercise;
 
 public class T3 {
 	public static void main(String[] args) {
-		System.out.println("1~10까지 출력");
-		for(int i=1; i<=10; i++)
+		int tot=0;
+		System.out.println("**** 1~10까지의 합을 출력하는 프로그램");
+		for(int i=1; i<=10; i++) {
 			System.out.println(" i " + i);
+		  tot += i;
+	 }
 	}
 }
