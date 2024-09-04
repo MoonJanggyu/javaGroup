ab79de156c873cce6ffb3f3dac548d49ee5054ac
green <green@DESKTOP-AKPHHR4>
2024-09-04 14:34:25 +0900

    20240904_1534 real last update

--- src/t50_Exercise/T6.java
+++ src/t50_Exercise/T6.java
@@ -2,6 +2,16 @@ package t50_Exercise;
 
 public class T6 {
 	public static void main(String[] args) {
-		System.out.println("안녕히계세요!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		System.out.println("안녕히계세요 여러분!!!");
+		
 	}
 }
