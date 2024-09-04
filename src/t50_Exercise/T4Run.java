7435622007ccfd141c38ca181ed0efaecdd5b01b
green <green@DESKTOP-AKPHHR4>
2024-09-04 14:07:31 +0900

    20240904_1407 T4.java 더하기 메소드

---
+++ src/t50_Exercise/T4Run.java
@@ -0,0 +1,12 @@
+package t50_Exercise;
+
+public class T4Run {
+	public static void main(String[] args) {
+		new T4();
+			T4 t4 = new T4();
+		
+		int res = t4.add(10, 20);
+		
+		System.out.println("res : " + res);
+	}
+}
