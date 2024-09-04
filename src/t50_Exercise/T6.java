462358399a29db174de2655600fd9df4e6d98616
MoonJanggyu <bricuerp@gmail.com>
2024-09-04 14:31:44 +0900

    2024

--- src/t50_Exercise/T6.java
+++ src/t50_Exercise/T6.java
@@ -3,5 +3,9 @@ package t50_Exercise;
 public class T6 {
 	public static void main(String[] args) {
 		System.out.println("안녕히계세요!!!");
+		System.out.println("안녕히계세요!!!");
+		System.out.println("안녕히계세요!!!");
+		System.out.println("안녕히계세요!!!");
+		System.out.println("안녕히계세요!!!");
 	}
 }
