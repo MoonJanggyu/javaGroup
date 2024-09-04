c56f00f1646e63ead0619fcc530d70010175002b
MoonJanggyu <bricuerp@gmail.com>
2024-09-04 14:41:10 +0900

    20240904_1441 last dance

--- src/t50_Exercise/T6.java
+++ src/t50_Exercise/T6.java
@@ -9,7 +9,8 @@ public class T6 {
 		System.out.println("안녕히계세요!!!");
 		System.out.println("안녕히계세요!!!");
 		System.out.println("안녕히계세요!!!");
-		System.out.println();
->>>>>>> branch 'master' of https://github.com/MoonJanggyu/javaGroup.git
+		
+		System.out.println("last dance");
+		
 	}
 }
